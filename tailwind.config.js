51639017cfd493f0321eb59bbd181da8a2cb7074
crnorthc <crnorthc@eckerd.edu>
2021-09-10 19:27:34 -0400

    final edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,25 @@
-module.exports = {
- purge: [],
- darkMode: false, // or 'media' or 'class'
- theme: {
-  colors: {
-   light: "#eeeeee",
-   medium: "#344246",
-   medium50: "#34424679",
-   dark: "#1E292A",
-   primary: "#FFB900",
-   secondary: "#00C2EF",
-  },
- },
+const defaultTheme = require('tailwindcss/defaultTheme')
 
- variants: {
-  extend: {},
- },
- plugins: [],
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        screens: {
+            'pics': "1080px",
+            ...defaultTheme.screens
+        },
+        colors: {
+            light: "#eeeeee",
+            medium: "#344246",
+            medium50: "#34424679",
+            dark: "#1E292A",
+            primary: "#FFB900",
+            secondary: "#00C2EF",
+        },
+    },
+
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
