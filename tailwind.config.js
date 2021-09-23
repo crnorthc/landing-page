62ab8522b989f8236b4565f290d135ca2c4840bc
Matthew <matthew.j.klimek@gmail.com>
2021-09-23 13:03:24 -0400

    adding lineup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,32 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        screens: {
-            'pics': "1080px",
-            ...defaultTheme.screens
-        },
-        colors: {
-            light: "#eeeeee",
-            medium: "#344246",
-            medium50: "#34424679",
-            dark: "#1E292A",
-            primary: "#FFB900",
-            secondary: "#00C2EF",
-        },
-    },
+ purge: [],
+ darkMode: false, // or 'media' or 'class'
+ theme: {
+  screens: {
+   pics: "1080px",
+   ...defaultTheme.screens,
+  },
+  colors: {
+   light: "#eeeeee",
+   lightmedium: "#8e8e8e",
+   medium: "#344246",
+   medium50: "#34424679",
+   dark: "#1E292A",
+   primary: "#FFB900",
+   secondary: "#00C2EF",
+   red: "#FF0000",
+   green: "#008000",
+   blue: "#0000ff",
+   skyblue: "#209fff",
+  },
+ },
+ variants: {
+  extend: {
+   backgroundColor: ["even"],
+  },
+ },
 
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+ plugins: [],
 };
