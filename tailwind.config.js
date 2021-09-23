69b2f43a91a274016fa62d3c4c4f991ee5d00297
crnorthc <crnorthc@eckerd.edu>
2021-09-23 14:00:24 -0400

    oopsie

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,34 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
 
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        screens: {
-            'pics': "1080px",
-            ...defaultTheme.screens
-        },
-        colors: {
-            light: "#eeeeee",
-            medium: "#344246",
-            medium50: "#34424679",
-            dark: "#1E292A",
-            primary: "#FFB900",
-            secondary: "#00C2EF",
-        },
+     screens: {
+      pics: "1080px",
+      ...defaultTheme.screens,
+     },
+     colors: {
+      light: "#eeeeee",
+      lightmedium: "#8e8e8e",
+      medium: "#344246",
+      medium50: "#34424679",
+      dark: "#1E292A",
+      primary: "#FFB900",
+      secondary: "#00C2EF",
+      red: "#FF0000",
+      green: "#008000",
+      blue: "#0000ff",
+      skyblue: "#209fff",
+      greenHue: "#28a74542",
+      redHue: "#FF000042",
+     },
     },
-
     variants: {
-        extend: {},
+     extend: {
+      backgroundColor: ["even"],
+     },
     },
+   
     plugins: [],
-};
+   };
